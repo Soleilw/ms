de00431de470ca0997c609056cb01519f0613fd0
soleil <a617759082@126.com>
2020-12-04 10:38:34 +0800

    update

--- src/views/device/command.vue
+++ src/views/device/command.vue
@@ -20,7 +20,8 @@
 			</div>
 
 			<div class="content-box-right">
-				<el-table :data="commandsData" stripe="" border :header-cell-style="{background:'#f0f0f0', color: '#003366'}" :max-height="700">
+				<el-table :data="commandsData" stripe="" border :header-cell-style="{background:'#f0f0f0', color: '#003366'}"
+				 :max-height="700">
 					<el-table-column prop="id" label="ID"></el-table-column>
 					<el-table-column prop="device_uuid" label="设备ID" width="200px"></el-table-column>
 					<el-table-column prop="command" label="指令"></el-table-column>
@@ -68,7 +69,7 @@
 					</el-form-item>
 					<el-form-item label="设备uuid">
 						<el-select v-model="command_uuid" placeholder="请选择uuid" filterable @change="uuidChange">
-							<el-option v-for="(item,index) in uuidList" :key="index" :label="item.uuid" :value="item.uuid">
+							<el-option v-for="(item,index) in uuidList" :key="index" :label="item.uuid/item.remark" :value="item.uuid">
 							</el-option>
 						</el-select>
 					</el-form-item>
@@ -112,7 +113,28 @@
 							<el-input v-model="commandform.url"></el-input>
 						</el-form-item>
 					</div>
-					<div style="margin-left: 100px;">
+					<!-- 设置屏保 -->
+					<div v-if="command === 'setScreenSave' ">
+						<el-form-item label="屏保时间">
+							<el-time-picker is-range v-model="screenTime" range-separator="至" start-placeholder="屏保开始时间" end-placeholder="屏保结束时间"
+							 placeholder="选择时间范围" :value-format="timeScreen" @change="screenTimeChange">
+							</el-time-picker>
+						</el-form-item>
+						<el-form-item label="图片切换间隔时间">
+							<el-input-number v-model="intervalTime" @change="intervalTimeChange"></el-input-number>
+						</el-form-item>
+						<el-form-item label="人离开后进入屏保的时间">
+							<el-input-number v-model="leaveTime" @change="leaveTimeChange"></el-input-number>
+						</el-form-item>
+						<el-form-item label="上传屏保图片">
+							<el-upload action="https://upload-z2.qiniup.com" ref="upload" :limit="5" :before-upload="beforeAvatarUpload"
+							 :auto-upload="false" :on-success="handleAvatarSuccess" :on-remove="handleRemove" :on-exceed="handleExceed"
+							 :on-change="handleChange" :data="imgData" multiple list-type="picture-card">
+								<el-button size="small" type="primary">选择图片</el-button>
+							</el-upload>
+						</el-form-item>
+					</div>
+					<div  class="submit">
 						<el-button type="primary" @click="sendCommand">发送</el-button>
 					</div>
 				</el-form>
@@ -124,6 +146,9 @@
 
 <script>
 	import API from '@/api/index.js'
+	import axios from 'axios'
+	import md5 from 'blueimp-md5'
+
 	export default {
 		name: 'gradems',
 		data() {
@@ -143,6 +168,24 @@
 				valueFormatTime: 'yyyy-MM-dd HH:mm:ss',
 				more_data: '', // 查看更多内容的数据
 				commandList: [],
+
+				// 上传人脸
+				imgData: {
+					key: '',
+					token: ''
+				},
+				fileName: '',
+				suffix: '',
+				qiniuaddr: "https://tu.fengniaotuangou.cn", // 七牛云图片外链地址
+				screenSaverList: [], // 上传屏保
+				// 屏保开始时间
+				screenTime: '',
+				timeScreen: 'HH:mm', // 时间格式
+				intervalTime: 0, // 间隔时间
+				leaveTime: '',
+
+				screenFrom: {}, // 指令为屏保时候输入
+				submitChanner: false, // 提交通道
 
 				pro_city_area: [], // 根据省市区搜索
 				pro_city_area_id: '', // 根据社区id搜索
@@ -188,7 +231,8 @@
 				// 分页
 				current: 1, // 当前页
 				size: 10, // 每页出现几条
-				total: 0 // 总页数
+				total: 0, // 总页数
+
 			}
 		},
 		mounted() {
@@ -274,6 +318,21 @@
 				console.log(this.userAuthTime)
 			},
 
+			screenTimeChange(val) {
+				var self = this;
+				self.screenFrom.start = val[0];
+				self.screenFrom.end = val[1];
+			},
+
+			intervalTimeChange(val) {
+				var self = this;
+				self.screenFrom.interval_time = val;
+			},
+			leaveTimeChange(val) {
+				var self = this;
+				self.screenFrom.start_time = val;
+			},
+
 			changeCommand(val) {
 				var self = this;
 				self.commandform = {};
@@ -315,6 +374,11 @@
 							url: ''
 						}
 						break;
+					case 'setScreenSave':
+						self.screenFrom.uuid = self.command_uuid;
+						self.screenFrom.command = 'setScreenSave';
+						console.log(self.screenFrom)
+						break;
 						// case 'getSoftVersion':
 						// 	self.commandform = {
 						// 		uuid: self.command_uuid,
@@ -350,6 +414,7 @@
 			// 发送指令
 			sendCommand() {
 				var self = this;
+				self.$refs.upload.submit();
 				if (self.command) {
 					switch (self.command) {
 						case "setPassword":
@@ -407,6 +472,37 @@
 								self.getCommands();
 							})
 							break;
+						case 'setScreenSave':
+							console.log(self.screenFrom)
+							console.log(self.submitChanner)
+							if(self.submitChanner) {
+								self.$refs.upload.submit();
+								API.sendDeviceCommand(self.screenFrom).then(res => {
+									self.$message.success("发送成功");
+									self.command_uuid = '';
+									self.command = '';
+									self.screenTime = '',
+									self.intervalTime = 0, // 间隔时间
+									self.leaveTime = '',
+									self.screenFrom = {},
+									self.screenSaverList = [],
+									self.dialogCommand = false;
+									self.getCommands();
+								})
+							} else {
+								API.sendDeviceCommand(self.screenFrom).then(res => {
+									self.$message.success("发送成功");
+									self.command_uuid = '';
+									self.command = '';
+									self.screenTime = '',
+									self.intervalTime = 0, // 间隔时间
+									self.leaveTime = '',
+									self.screenFrom = {},
+									self.dialogCommand = false;
+									self.getCommands();
+								})
+							}
+							break;
 							// case 'getSoftVersion':
 							// 	API.sendDeviceCommand(self.commandform).then(res => {
 							// 		self.$message.success("发送成功");
@@ -457,17 +553,111 @@
 				} else {
 					self.$message.warning('请选择指令！');
 				}
-
 			},
 			// 关闭指令uuid
 			closeCommands() {
 				var self = this;
-				console.log('111uuid', self.command_uuid)
 				self.command_uuid = '';
 				self.command = '';
 			},
 
-	// 重新筛选
+			// 上传屏保图片
+			getQiniuToken() {
+				var self = this;
+				axios.get('https://api.fengniaotuangou.cn/api/upload/token').then(res => {
+					self.imgData.token = res.data.uptoken;
+				})
+			},
+			// 移除照片
+			handleRemove(file, fileList) {
+				console.log(file, fileList)
+				if(fileList.length == 0) {
+					self.submitChanner = false;
+				}
+			},
+			// 改变图片
+			handleChange(file, fileList) {
+				var self = this;
+				console.log(file, fileList)
+				if(fileList.length > 0) {
+					self.submitChanner = true;
+				}
+			},
+			// 上传前
+			beforeAvatarUpload(file, fileList) {
+				var self = this;
+				console.log(file, fileList)
+				self.getQiniuToken();
+				// self.familyForm.href = file.name;
+				self.fileName = md5(file.name);
+				self.suffix = file.name.substring(file.name.lastIndexOf('.') + 1);
+				self.imgData.key = `tmp_${self.fileName}.${self.suffix}`
+				// 上传之前后,把图片推进screenSaverList
+				var _screenSaverList = [];
+				if (self.imgData.key) {
+					self.screenSaverList.push(`${self.qiniuaddr}/${self.imgData.key}`)
+					for(var i = 0; i < self.screenSaverList.length; i++) {
+						if(_screenSaverList.indexOf(self.screenSaverList[i]) === -1) {
+							_screenSaverList.push(self.screenSaverList[i])
+						}
+					}
+				}
+				for (let i = 0; i < _screenSaverList.length; i++) {
+					if (i == 0) {
+						self.screenFrom.image_1 = self.screenSaverList[i]
+					} else if (i == 1) {
+						self.screenFrom.image_2 = self.screenSaverList[i]
+					} else if (i == 2) {
+						self.screenFrom.image_3 = self.screenSaverList[i]
+					} else if (i == 3) {
+						self.screenFrom.image_4 = self.screenSaverList[i]
+					} else if (i == 4) {
+						self.screenFrom.image_5 = self.screenSaverList[i]
+					}
+					console.log(self.screenFrom)
+				}
+			},
+			// 上传成功
+			handleAvatarSuccess(res, file, fileList) {
+				var self = this;
+				console.log(111, res)
+				console.log(222, fileList)
+				
+				// for (let i = 0; i < self.screenSaverList.length; i++) {
+				// 	if (i == 0) {
+				// 		self.screenFrom.image_1 = self.screenSaverList[i]
+				// 	} else if (i == 1) {
+				// 		self.screenFrom.image_2 = self.screenSaverList[i]
+				// 	} else if (i == 2) {
+				// 		self.screenFrom.image_3 = self.screenSaverList[i]
+				// 	} else if (i == 3) {
+				// 		self.screenFrom.image_4 = self.screenSaverList[i]
+				// 	} else if (i == 4) {
+				// 		self.screenFrom.image_5 = self.screenSaverList[i]
+				// 	}
+				// 	console.log(self.screenFrom)
+				// 	debugger
+				// }
+				// API.sendDeviceCommand(self.screenFrom).then(res => {
+				// 	debugger
+				// 	self.$message.success("发送成功");
+				// 	self.command_uuid = '';
+				// 	self.command = '';
+				// 	self.screenTime = '',
+				// 	self.intervalTime = 0, // 间隔时间
+				// 	self.leaveTime = '',
+				// 	self.screenFrom = {},
+				// 	self.dialogCommand = false;
+				// 	self.getCommands();
+				// })
+			},
+			// 上传数量
+			handleExceed() {
+				var self = this;
+				self.$message.error('上传图片不能超过5张');
+			},
+
+			// 重新筛选
 			resetSelect() {
 				window.location.reload();
 			},
